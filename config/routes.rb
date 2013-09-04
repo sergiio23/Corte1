33e162662b08735d848831add171b906d3e96a51
Sergio Ramirez <sergiio23@hotmail.com>
2013-09-04 00:19:00 -0500

    Creando Entidad User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Corte1::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
