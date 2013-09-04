b4d3599ed552d79f7a9dc89ed0abba534e3b7143
Sergio Ramirez <sergiio23@hotmail.com>
2013-09-03 23:53:50 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Corte1::Application.config.secret_key_base = '33e2c47e2efcc62c730d4f19580b096b5123eeb0aa483c5089d340b2edb6f64584d8c03041ea6afdbd076f3d9e214caab474e61c45ae789877cf9b9c5466232c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Corte1::Application.config.session_store :cookie_store, key: '_corte1_session'
